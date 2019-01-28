22d4072e3f5fbc8732f70fb0715ad0a83ea1524a
Rainbow-556 <903263494@qq.com>
2019-01-28 20:22:15 +0800

    1.NestedScroll header吸顶效果

--- app/src/main/java/com/practice/app/android/nestedscroll/NestedScrollLinearLayout.java
+++ app/src/main/java/com/practice/app/android/nestedscroll/NestedScrollLinearLayout.java
@@ -2,13 +2,15 @@ package com.practice.app.android.nestedscroll;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.NestedScrollingParent;
+import android.support.v4.view.NestedScrollingParent2;
 import android.support.v4.view.NestedScrollingParentHelper;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,23 +20,30 @@ import com.practice.app.R;
 /**
  * Created by glennli on 2019/1/24.<br/>
  */
-public final class NestedScrollLinearLayout extends LinearLayout implements NestedScrollingParent {
+public final class NestedScrollLinearLayout extends LinearLayout implements NestedScrollingParent2 {
     private TextView tvTitle;
-    private View mTabView, mTopView, mListView;
+    private View mTabView, mHeaderView, mTitleView, mContentView, mListView;
+    private ValueAnimator mAnimator;
     private NestedScrollingParentHelper mNestedScrollingParentHelper;
+    private int mHeaderViewId, mTabViewId, mTitleViewId, mTitleTextViewId, mContentViewId;
 
     public NestedScrollLinearLayout(Context context) {
-        super(context);
-        init(context);
+        this(context, null);
     }
 
     public NestedScrollLinearLayout(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
+        this(context, attrs, 0);
     }
 
     public NestedScrollLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.NestedScrollLinearLayout);
+        mHeaderViewId = array.getResourceId(R.styleable.NestedScrollLinearLayout_nsll_headerViewId, 0);
+        mTitleViewId = array.getResourceId(R.styleable.NestedScrollLinearLayout_nsll_titleViewId, 0);
+        mTitleTextViewId = array.getResourceId(R.styleable.NestedScrollLinearLayout_nsll_titleTextViewId, 0);
+        mTabViewId = array.getResourceId(R.styleable.NestedScrollLinearLayout_nsll_tabViewId, 0);
+        mContentViewId = array.getResourceId(R.styleable.NestedScrollLinearLayout_nsll_contentViewId, 0);
+        array.recycle();
         init(context);
     }
 
@@ -51,28 +60,29 @@ public final class NestedScrollLinearLayout extends LinearLayout implements Nest
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-        tvTitle = findViewById(R.id.tv_title);
-        mTabView = findViewById(R.id.ll_tab_container);
-        mTopView = findViewById(R.id.v_top);
+        mHeaderView = findViewById(mHeaderViewId);
+        mTitleView = findViewById(mTitleViewId);
+        tvTitle = findViewById(mTitleTextViewId);
+        mTabView = findViewById(mTabViewId);
+        mContentView = findViewById(mContentViewId);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        ViewPager pager = findViewById(R.id.pager);
         int scrollMeasureWidthSpec = MeasureSpec.makeMeasureSpec(
                 getMeasuredWidth() - getPaddingLeft() - getPaddingRight(), MeasureSpec.EXACTLY);
         // 重新设置list的高度
         int pagerHeight = getMeasuredHeight() - getPaddingTop() - getPaddingBottom()
-                - mTabView.getMeasuredHeight() - tvTitle.getMeasuredHeight();
+                - mTabView.getMeasuredHeight() - mTitleView.getMeasuredHeight();
         int scrollMeasureHeightSpec = MeasureSpec.makeMeasureSpec(
                 pagerHeight, MeasureSpec.EXACTLY);
-        pager.measure(scrollMeasureWidthSpec, scrollMeasureHeightSpec);
+        mContentView.measure(scrollMeasureWidthSpec, scrollMeasureHeightSpec);
     }
 
     @Override
     public void scrollTo(int x, int y) {
-        int topViewHeight = mTopView.getMeasuredHeight();
+        int topViewHeight = mHeaderView.getMeasuredHeight();
         if (y > topViewHeight) {
             y = topViewHeight;
         } else if (y < 0) {
@@ -82,73 +92,60 @@ public final class NestedScrollLinearLayout extends LinearLayout implements Nest
     }
 
     @Override
-    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes) {
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
         return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
     @Override
-    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes) {
-        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes);
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
+        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes, type);
     }
 
     @Override
-    public void onStopNestedScroll(@NonNull View target) {
-        mNestedScrollingParentHelper.onStopNestedScroll(target);
+    public void onStopNestedScroll(@NonNull View target, int type) {
+        mNestedScrollingParentHelper.onStopNestedScroll(target, type);
     }
 
     @Override
-    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
         // 子view滚动之后调用，会把滑动时没有消费的值传过来
         // 在这里处理为消费的值，可以解决list与悬停View之间的断层
-//        Log.w("lx", String.valueOf("onNestedScroll(): dyUnconsumed=" + dyUnconsumed));
-        boolean hiddenTop = dyUnconsumed > 0 && getScrollY() < mTopView.getMeasuredHeight();
-        boolean showTop = dyUnconsumed < 0 && getScrollY() > 0
-                && (!ViewCompat.canScrollVertically(mListView, -1) && !ViewCompat.canScrollVertically(target, -1));
-        if (hiddenTop || showTop) {
-            scrollBy(0, dyUnconsumed);
-        }
+        handleNestedScroll(target, dyUnconsumed, null);
     }
 
     @Override
-    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed) {
-//        Log.e("lx", "onNestedPreScroll: dy=" + String.valueOf(dy) + ", target=" + target.getClass().getSimpleName());
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
+        handleNestedScroll(target, dy, consumed);
+    }
+
+    private void handleNestedScroll(@NonNull View target, int dy, int[] consumed) {
         // dy > 0：手指向上滑，dy < 0：手指向下滑
-        boolean hiddenTop = dy > 0 && getScrollY() < mTopView.getMeasuredHeight();
+        boolean hiddenTop = dy > 0 && getScrollY() < mHeaderView.getMeasuredHeight();
         boolean showTop = dy < 0 && getScrollY() > 0
                 && (!ViewCompat.canScrollVertically(mListView, -1) && !ViewCompat.canScrollVertically(target, -1));
         if (hiddenTop || showTop) {
-            consumed[1] = dy;
+            if (consumed != null) {
+                consumed[1] = dy;
+            }
+            // 0 ~ 1
+            float percent = 1f * (getScrollY() + dy) / mHeaderView.getMeasuredHeight();
+            percent = Math.max(0, percent);
+            percent = Math.min(1, percent);
+            Log.e("lx", String.valueOf(percent));
+            // 最小缩放到0.7
+            percent = 1 - 0.3f * percent;
+            tvTitle.setScaleX(percent);
+            tvTitle.setScaleY(percent);
             scrollBy(0, dy);
         }
     }
 
     @Override
-    public boolean onNestedFling(@NonNull View target, float velocityX, float velocityY, boolean consumed) {
-        // velocityY > 0：手指向上滑，velocityY < 0：手指向下滑
-//        Log.e("lx", String.valueOf("onNestedFling(): velocityY=" + velocityY));
-        return false;
-    }
-
-    @Override
-    public boolean onNestedPreFling(@NonNull View target, float velocityX, float velocityY) {
-        boolean hiddenTop = velocityY > 0 && getScrollY() < mTopView.getMeasuredHeight();
-        boolean showTop = velocityY < 0 && getScrollY() > 0
-                && (!ViewCompat.canScrollVertically(mListView, -1) && !ViewCompat.canScrollVertically(target, -1));
-        if (hiddenTop || showTop) {
-            animateScroll(velocityY, false);
-        }
-        return false;
-    }
-
-    @Override
     public int getNestedScrollAxes() {
-        return 0;
+        return mNestedScrollingParentHelper.getNestedScrollAxes();
     }
 
-    private ValueAnimator mAnimator;
-
-    private void animateScroll(float velocityY, boolean consumed) {
-//        Log.e("lx", String.valueOf(consumed));
+    private void animateScroll(float velocityY) {
         if (mAnimator == null) {
             mAnimator = ValueAnimator.ofInt(0, 1);
             mAnimator.setDuration(150);
@@ -166,7 +163,7 @@ public final class NestedScrollLinearLayout extends LinearLayout implements Nest
         int start = -1, end = -1;
         if (velocityY > 0) {
             start = getScrollY();
-            end = mTopView.getMeasuredHeight();
+            end = mHeaderView.getMeasuredHeight();
         } else if (velocityY < 0) {
             start = getScrollY();
             end = 0;
--- app/src/main/java/com/practice/app/android/nestedscroll/NestedScrollLinearLayout1.java
+++ app/src/main/java/com/practice/app/android/nestedscroll/NestedScrollLinearLayout1.java
@@ -4,7 +4,7 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.NestedScrollingParent2;
+import android.support.v4.view.NestedScrollingParent;
 import android.support.v4.view.NestedScrollingParentHelper;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
@@ -18,23 +18,22 @@ import com.practice.app.R;
 /**
  * Created by glennli on 2019/1/24.<br/>
  */
-public final class NestedScrollLinearLayout2 extends LinearLayout implements NestedScrollingParent2 {
+public final class NestedScrollLinearLayout1 extends LinearLayout implements NestedScrollingParent {
     private TextView tvTitle;
     private View mTabView, mTopView, mListView;
-    private ValueAnimator mAnimator;
     private NestedScrollingParentHelper mNestedScrollingParentHelper;
 
-    public NestedScrollLinearLayout2(Context context) {
+    public NestedScrollLinearLayout1(Context context) {
         super(context);
         init(context);
     }
 
-    public NestedScrollLinearLayout2(Context context, @Nullable AttributeSet attrs) {
+    public NestedScrollLinearLayout1(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(context);
     }
 
-    public NestedScrollLinearLayout2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public NestedScrollLinearLayout1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context);
     }
@@ -83,51 +82,73 @@ public final class NestedScrollLinearLayout2 extends LinearLayout implements Nes
     }
 
     @Override
-    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes, int type) {
+    public boolean onStartNestedScroll(@NonNull View child, @NonNull View target, int axes) {
         return (axes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
     }
 
     @Override
-    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes, int type) {
-        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes, type);
+    public void onNestedScrollAccepted(@NonNull View child, @NonNull View target, int axes) {
+        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes);
     }
 
     @Override
-    public void onStopNestedScroll(@NonNull View target, int type) {
-        mNestedScrollingParentHelper.onStopNestedScroll(target, type);
+    public void onStopNestedScroll(@NonNull View target) {
+        mNestedScrollingParentHelper.onStopNestedScroll(target);
     }
 
     @Override
-    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+    public void onNestedScroll(@NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
         // 子view滚动之后调用，会把滑动时没有消费的值传过来
         // 在这里处理为消费的值，可以解决list与悬停View之间的断层
-        handleNestedScroll(target, dyUnconsumed, null);
+//        Log.w("lx", String.valueOf("onNestedScroll(): dyUnconsumed=" + dyUnconsumed));
+        boolean hiddenTop = dyUnconsumed > 0 && getScrollY() < mTopView.getMeasuredHeight();
+        boolean showTop = dyUnconsumed < 0 && getScrollY() > 0
+                && (!ViewCompat.canScrollVertically(mListView, -1) && !ViewCompat.canScrollVertically(target, -1));
+        if (hiddenTop || showTop) {
+            scrollBy(0, dyUnconsumed);
+        }
     }
 
     @Override
-    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed, int type) {
-        handleNestedScroll(target, dy, consumed);
-    }
-
-    private void handleNestedScroll(@NonNull View target, int dy, int[] consumed) {
+    public void onNestedPreScroll(@NonNull View target, int dx, int dy, @NonNull int[] consumed) {
+//        Log.e("lx", "onNestedPreScroll: dy=" + String.valueOf(dy) + ", target=" + target.getClass().getSimpleName());
         // dy > 0：手指向上滑，dy < 0：手指向下滑
         boolean hiddenTop = dy > 0 && getScrollY() < mTopView.getMeasuredHeight();
         boolean showTop = dy < 0 && getScrollY() > 0
                 && (!ViewCompat.canScrollVertically(mListView, -1) && !ViewCompat.canScrollVertically(target, -1));
         if (hiddenTop || showTop) {
-            if (consumed != null) {
-                consumed[1] = dy;
-            }
+            consumed[1] = dy;
             scrollBy(0, dy);
         }
     }
 
     @Override
-    public int getNestedScrollAxes() {
-        return mNestedScrollingParentHelper.getNestedScrollAxes();
+    public boolean onNestedFling(@NonNull View target, float velocityX, float velocityY, boolean consumed) {
+        // velocityY > 0：手指向上滑，velocityY < 0：手指向下滑
+//        Log.e("lx", String.valueOf("onNestedFling(): velocityY=" + velocityY));
+        return false;
     }
 
-    private void animateScroll(float velocityY) {
+    @Override
+    public boolean onNestedPreFling(@NonNull View target, float velocityX, float velocityY) {
+        boolean hiddenTop = velocityY > 0 && getScrollY() < mTopView.getMeasuredHeight();
+        boolean showTop = velocityY < 0 && getScrollY() > 0
+                && (!ViewCompat.canScrollVertically(mListView, -1) && !ViewCompat.canScrollVertically(target, -1));
+        if (hiddenTop || showTop) {
+            animateScroll(velocityY, false);
+        }
+        return false;
+    }
+
+    @Override
+    public int getNestedScrollAxes() {
+        return 0;
+    }
+
+    private ValueAnimator mAnimator;
+
+    private void animateScroll(float velocityY, boolean consumed) {
+//        Log.e("lx", String.valueOf(consumed));
         if (mAnimator == null) {
             mAnimator = ValueAnimator.ofInt(0, 1);
             mAnimator.setDuration(150);
