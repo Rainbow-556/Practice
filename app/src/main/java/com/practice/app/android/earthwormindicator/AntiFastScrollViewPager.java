78021236ebeddb4caf9d9a9fa826ee94d0be3bd1
Rainbow-556 <903263494@qq.com>
2019-02-26 13:22:06 +0800

    1.ViewPager蚯蚓指示器

--- app/src/main/java/com/practice/app/android/earthwormindicator/AntiFastScrollViewPager.java
+++ app/src/main/java/com/practice/app/android/earthwormindicator/AntiFastScrollViewPager.java
@@ -6,6 +6,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by lixiang on 2019/2/26.
@@ -24,6 +29,8 @@ public final class AntiFastScrollViewPager extends ViewPager {
     }
 
     private void init() {
+        // 屏蔽快速滑动翻页，让手指抬起时，ViewPager滚动到指定位置时快一点，缩短屏蔽快速滚动的时间
+        setSliderTransformDuration(this);
         addOnPageChangeListener(new OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -50,5 +57,42 @@ public final class AntiFastScrollViewPager extends ViewPager {
             return true;
         }
         return super.dispatchTouchEvent(ev);
+    }
+
+    private static void setSliderTransformDuration(ViewPager viewPager) {
+        try {
+            Field mScroller = ViewPager.class.getDeclaredField("mScroller");
+            mScroller.setAccessible(true);
+            FixedSpeedScroller scroller = new FixedSpeedScroller(viewPager.getContext(), new AccelerateDecelerateInterpolator());
+            mScroller.set(viewPager, scroller);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class FixedSpeedScroller extends Scroller {
+        private float durationRatio = 1.9f;
+
+        public FixedSpeedScroller(Context context) {
+            super(context);
+        }
+
+        public FixedSpeedScroller(Context context, Interpolator interpolator) {
+            super(context, interpolator);
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            // Ignore received duration, use fixed one instead
+//            super.startScroll(startX, startY, dx, dy, duration);
+            super.startScroll(startX, startY, dx, dy, (int) (duration / durationRatio));
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy) {
+            // Ignore received duration, use fixed one instead
+//            super.startScroll(startX, startY, dx, dy, duration);
+            super.startScroll(startX, startY, dx, dy, (int) (350 / durationRatio));
+        }
     }
 }
